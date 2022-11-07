7ee23e556020865241b31868753c4be0bdb7b035
Abdellah Slimani <slimaniabdellah.as@gmail.com>
2022-11-07 18:01:18 +0100

    Intial commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import css from './Header.module.css'
+
+function Header() {
+  return (
+    <div className={css.container}>
+        
+    </div>
+  )
+}
+
+export default Header
