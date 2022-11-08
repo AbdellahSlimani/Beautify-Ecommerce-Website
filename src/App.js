6323ab6e239dd22bd2282019acd50142261f3a8a
Abdellah Slimani <slimaniabdellah.as@gmail.com>
2022-11-08 17:55:57 +0100

    Add a Virtual try section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
-import Header from "./components/header/Header";
-import Hero from "./components/hero/Hero";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Slider from "./components/Slider/Slider";
+import Virtual from "./components/Virtual/Virtual";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
+      <Slider />
+      <Virtual />
     </div>
   );
 }
---
+++ src/components/Virtual/Virtual.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import css from "./Virtual.module.css";
+import Shade from "../../assets/shade.png";
+import ReactCompareImage from "react-compare-image";
+import theBeforeImg from "../../assets/before.png";
+import theAfterImg from "../../assets/after.png";
+
+function Virtual() {
+  return (
+    <div className={css.Virtual}>
+      <div className={css.left}>
+        <span>Virtual Try-on</span>
+        <span>Never Buy the wrong Shade Again!</span>
+        <span>Try Now!</span>
+        <img src={Shade} alt="decoration shade" />
+      </div>
+      <div className={css.right}>
+        <div className={css.compareImgWrapper}>
+          <ReactCompareImage
+            leftImage={theBeforeImg}
+            rightImage={theAfterImg}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Virtual;
